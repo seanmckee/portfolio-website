e324ede6f67a8ac87f3926736c01fd23ab545db7
seanmckee <seanmingmckee@gmail.com>
2023-02-10 16:47:06 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import About from './components/About'
+import Contact from './components/Contact'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Skills from './components/Skills'
+import Work from './components/Work'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Work />
+      <Contact />
+    </div>
+  )
+}
+
+export default App
