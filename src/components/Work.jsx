e55043d5c55f1dceff2594df30a95f40c5393bef
seanmckee <seanmingmckee@gmail.com>
2023-05-25 07:34:22 -0700

    updated links

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,81 +1,79 @@
-import React from 'react'
-import WorkImg from '../assets/projects/workImg.jpeg'
-import RealEstate from '../assets/projects/realestate.jpg'
+import React from "react";
+import WorkImg from "../assets/projects/workImg.jpeg";
+import RealEstate from "../assets/projects/realestate.jpg";
+import ValorantTrivia from "../assets/projects/valorantTrivia.png";
+import RealEstateGif from "../assets/projects/RealEstateGif.gif";
 
 const Work = () => {
   return (
-    <div name="work" className='w-full md:h-screen text-gray-300 bg-[#0a192f] '>
-        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Work</p>
-                <p className='py-6'>// Check out some of my recent work</p>
-            </div>
-            {/* Container */}
-            <div  
-            className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-                
-
-                {/* grid item */}
-                <div style={{backgroundImage: `url(${WorkImg})`}} 
-                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                    
-                    {/* hover effects */}
-                    <div className='opacity-0 group-hover:opacity-100'> 
-                        <span className='text-2xl font-bold text-white tracking-wider'>
-                           React JS Application 
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                            </a>
-                            <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div style={{backgroundImage: `url(${RealEstate})`}} 
-                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                    
-                    {/* hover effects */}
-                    <div className='opacity-0 group-hover:opacity-100'> 
-                        <span className='text-2xl font-bold text-white tracking-wider'>
-                           React JS Application 
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                            </a>
-                            <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div style={{backgroundImage: `url(${WorkImg})`}} 
-                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                    
-                    {/* hover effects */}
-                    <div className='opacity-0 group-hover:opacity-100'> 
-                        <span className='text-2xl font-bold text-white tracking-wider'>
-                           React JS Application 
-                        </span>
-                        <div className='pt-8 text-center'>
-                            <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                            </a>
-                            <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f] ">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+            Work
+          </p>
+          <p className="py-6">// Check out some of my recent work</p>
         </div>
-    </div>
-  )
-}
+        {/* Container */}
+        <div className="grid sm:grid-cols-2 gap-4">
+          {/* grid item */}
+          <div
+            style={{ backgroundImage: `url(${ValorantTrivia})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Valorant Trivia
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://valorant-trivia.netlify.app/" target="_blank">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/seanmckee/Valorant-Trivia"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
 
-export default Work
+          <div
+            style={{ backgroundImage: `url(${RealEstateGif})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Real Estate Website
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://realestate-sm.netlify.app/" target="_blank">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/seanmckee/Real-Estate-Site"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
